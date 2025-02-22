90e0036ce5a194173102413cf7821241183e3f99
Prateek Giri <prateekgiri999@gmail.com>
2025-02-22 15:30:09 +0530

    lec04

--- src/main/java/com/Security/SecurityLearning/config/security/filters/CustomAuthenticationFilter.java
+++ src/main/java/com/Security/SecurityLearning/config/security/filters/CustomAuthenticationFilter.java
@@ -27,7 +27,7 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-          //1. create an authentication  object   which is not  yet authenticated
+          // 1. create an authentication  object   which is not  yet authenticated
          // 2. delegate  the authentication object  to manager
         //  3. get back the authentication from manager
        //   4. if the object is authenticated then send  to the next filter in the chain
